c6e6bfe115c1da33b576aaf705e1beb514f1ba28
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-07-20 20:20:54 +0300

    task1fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,9 @@
 const categoriesRef = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesRef.length}`);
 
-const titleNameRef = document.querySelectorAll("h2");
-const amountItemRef = document.querySelectorAll("li.item > ul");
-
-console.log(`Category: ${titleNameRef[0].textContent}`);
-console.log(`Elements: ${amountItemRef[0].children.length}`);
-
-console.log(`Category: ${titleNameRef[1].textContent}`);
-console.log(`Elements: ${amountItemRef[1].children.length}`);
-
-console.log(`Category: ${titleNameRef[2].textContent}`);
-console.log(`Elements: ${amountItemRef[2].children.length}`);
+categoriesRef.forEach(item => {
+const titleNameRef = item.querySelector("h2");
+    const amountItemRef = item.querySelectorAll("li");
+    console.log(`Category: ${titleNameRef.textContent}`);
+    console.log(`Elements: ${amountItemRef.length}`);
+})
